2417ff67b7276dd28abd4cdf55832ad2538561d7
amein abdi <ameinabdi@gmail.com>
2018-09-07 08:52:47 +0300

    database fix

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'adalsite'
+  host     : 'us-cdbr-iron-east-01.cleardb.net',
+  user     : 'b9941530b8e7a0',
+  password : 'c840afcb',
+  database : 'heroku_bd26221b1bd4a86'
 });
 connection.connect(function(err){
 if(!err) {

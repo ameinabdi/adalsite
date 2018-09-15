742ab02ec5bce54e25043fe1b6e91ebf550a4812
amein abdi <adalsite17@gmail.com>
2018-09-15 17:40:08 +0300

    fix order

--- controls/costumers-control.js
+++ controls/costumers-control.js
@@ -14,13 +14,13 @@ module.exports.register = function(req, res){
    var  birthday = req.body.birthday;
 
 if( !fullname  || !email  || !password  || !gender  || !phone  || !phone  || !address  || !birthday) {
-    res.status(200).json(" fadlan waxbaad ka tagtey formka! buuxi")
+    res.json( 500," fadlan waxbaad ka tagtey formka! buuxi")
 }
 
  var sql = "INSERT INTO `costumers`(`fullname`,`email`,`password`,`gender`, `phone` ,`address`,`birthday`) VALUES ('" + fullname + "','" + email + "','" + password + "','" + gender + "','" + phone + "','" + address + "','" + birthday + "')";
  var query = connection.query(sql, function(err, result){
                 
-               res.status(500).json("waad ku guuleysatey is diwan gelinta")
+               res.json(200,"waad ku guuleysatey is diwan gelinta")
         })
 
  

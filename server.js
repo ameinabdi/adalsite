53983bfebb98bc0f2b453e6beb8b7e3e31624572
amein abdi <adalsite17@gmail.com>
2018-10-09 23:46:58 +0300

    fix hash

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ app.post('/addnotification', Notification.addnatification)
 
 
  
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 443;
 
 app.listen(port);
 console.log(ip.address())
